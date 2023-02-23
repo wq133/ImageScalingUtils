217cd6d841f42ab5a406bd67abb287840da7e884
wq133.github.io <wqh666@vip.qq.com>
2023-02-23 20:52:35 +0800

    更新

--- image_scaling/src/main/java/com/jiniu/image/demo/ImageScalingDemo.java
+++ image_scaling/src/main/java/com/jiniu/image/demo/ImageScalingDemo.java
@@ -3,6 +3,7 @@ package com.jiniu.image.demo;
 import org.apache.poi.util.IOUtils;
 import org.junit.Test;
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 
 import com.jiniu.image.utils.ImageUtils;
@@ -13,7 +14,18 @@ import com.jiniu.image.utils.ImageUtils;
  * @date 2022/12/16 14:27
  */
 public class ImageScalingDemo {
+    @Test
+    public static void testInByte(String[] args) throws IOException {
+        File srcFile = new File("C:/Users/wqh66/Pictures/Links/2022-06-21_175557.png");
+        ImageUtils.compressImageInByte(srcFile);
+    }
+    @Test
+    public void compareTwo() {
+        String fileNameTwo = "file_1672900830000手机银行交医保.jpg";
+        ImageUtils.imageScalingtwo(new File(fileNameTwo));
+        ImageUtils.imageScaling(new File(fileNameTwo));
 
+    }
     @Test
     public void scalingTest(){
         //String path = "C:/Users/wqh66/Desktop/rApk3WNPbmGATB-TAAdFIyh5gr0633.jpg";
@@ -30,14 +42,12 @@ public class ImageScalingDemo {
         try {
             // <1440*1080
             URL url = new URL("https://we-saas-test.jiniutech.com/file/group1/M00/00/01/rApk3WNOTE6AJqf4AAAfA22Kaps069.png");
-
             // >1440*1080
             //URL url = new URL("https://we-saas-cloud-cos.jiniutech.com/saas/dev/2022/11/30/93c7fc7a-cd26-488f-9d9f-f3c57ccaae41.jpg");
             //File file = ImageUtils.downloadAndImageScaling(url);
             //File file = ImageUtils.downloadAndDel(url);
             byte[] bytes = ImageUtils.downloadAndDel(url);
 //            file.delete();
-
             System.out.println("bytes = " + bytes);
             //Boolean aBoolean = ImageUtils.delCacheImage(file);
         } catch (Exception e) {
@@ -47,16 +57,8 @@ public class ImageScalingDemo {
 
     @Test
     public void testTwo() {
-//        String fileName = "IMG_20221208_210617.jpg";
-//        ImageUtils.imageScalingtwo(new File(fileName));
-//        String fileNameTwo = "e02c7ea64acce0cd749c4d5b50d9722e.jpeg";
-//        ImageUtils.imageScalingtwo(new File(fileNameTwo));
-//        String fileNameTwo = "企业微信截图_16732492972574.png";
-//        ImageUtils.imageScalingtwo(new File(fileNameTwo));
         String fileNameTwo = "file_1672900830000手机银行交医保.jpg";
         ImageUtils.imageScalingtwo(new File(fileNameTwo));
-//        ImageUtils.imageScaling(new File(fileNameTwo));
-
     }
 
 
--- image_scaling/src/main/java/com/jiniu/image/utils/ImageUtils.java
+++ image_scaling/src/main/java/com/jiniu/image/utils/ImageUtils.java
@@ -130,50 +130,93 @@ public class ImageUtils {
                     (double)1440/read.getWidth() : (double)1080/read.getHeight();
             int width = (int) (readWidth * scale);
             int height = (int) (readHeight * scale);
+            Double scale2 = 0.98;
             // 缩放完成，若还有一边超出范围 继续缩放
-            while (width > 1440 || height > 1080){
-                width = (int) (width * scale);
-                height = (int) (height * scale);
+            while (width*height > 1440 * 1080){
+                width = (int) (width * scale2);
+                height = (int) (height * scale2);
             }
+            // --------------------cv核心代码段start--------------
+            // 调用缩放后的图片 newImage
+            Image newImage = read.getScaledInstance(width, height, SCALE_AREA_AVERAGING);
+            // 创建新的缓存图片 bufferImage
+            BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+            // 获取画笔 copycode
+            Graphics2D graphics = bufferedImage.createGraphics();
+            // 将Image对象画在画布上,最后一个参数,ImageObserver:接收有关 Image 信息通知的异步更新接口,没用到直接传空
+            graphics.drawImage(newImage, 0, 0,null);
+            // 一定要释放资源
+            graphics.dispose();
+            // --------------------cv核心代码段end--------------
+
+            // 获取文件名and类型后缀
+            String name = file.getName();
+            String formateName = name.substring((name.indexOf(".") + 1));
+            // 获取pathName 直接传name的不会包含path
+            String pathName = (!file.getPath().contains("\\")) ?
+                    System.getProperty("user.dir") : Arrays.stream(file.getPath().split(file.getName())).collect(Collectors.toList()).get(0);
+            //System.out.println("pathName:"+pathName);
+
+            // 根据pathName判断是本地上传还是url上传
+            File copyFile = (pathName.contains(System.getProperty("user.dir"))) ?
+                    new File("scaling3_" +name) : new File(pathName+"scaling_" +name);
+
+            // 核心代码 bufferedImage写入copyFile
+            ImageIO.write(bufferedImage,formateName, copyFile);
+            return copyFile;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    /**
+     * 本地图片缩放
+     * 1.等比缩放
+     * 2.本地图片orUrl图片
+     * */
+    public static File imageScaling0(File file) {
+        try {
             // 初始化 固定缩放比例
-//            Double scales = 0.98;
-//            Double x = null;
-//            Double y = null;
-//            // 初始化宽高
-//            int readWidth = read.getWidth();
-//            int readHeight = read.getHeight();
-//            // 初始化二次缩放比例
-//
-//            Double scale = null;
-//
-//            // 首次缩放
-//            int width = (int) (readWidth * scale);
-//            int height = (int) (readHeight * scale);
-//            // 宽高比 面积 limit
-//            Double area = Double.valueOf(width * height);
-//            Double wh = Double.valueOf(width / height);
-//            Double limit = Double.valueOf(1440 * 1080);
-//
-//            // 缩放完成，若还有一边超出范围 继续缩放
-//            while (area > limit) {
-//                // 二次缩放
-//                scales = limit / area;
-//                BigDecimal bigDecimal = new BigDecimal(scales);
-//                scales = bigDecimal.setScale(4, BigDecimal.ROUND_HALF_UP).doubleValue();
-//
-//                x = Math.sqrt(scales / wh);
-//                bigDecimal = new BigDecimal(x);
-//                x = bigDecimal.setScale(4, BigDecimal.ROUND_HALF_UP).doubleValue();
-//
-//
-//                y = Math.sqrt(scales / wh);
-//                bigDecimal = new BigDecimal(y);
-//                y = bigDecimal.setScale(4, BigDecimal.ROUND_HALF_UP).doubleValue();
-//
-//                width *= (int) Math.floor(x * wh);
-//                height *= (int) Math.floor(y);
-//                area = Double.valueOf(width*height);
-//            }
+            Double scales = 0.98;
+            // 读取文件到bufferImag
+            BufferedImage read = ImageIO.read(file);
+            Double x = null;
+            Double y = null;
+            // 初始化宽高
+            int readWidth = read.getWidth();
+            int readHeight = read.getHeight();
+            // 初始化二次缩放比例
+            Double scale = null;
+            // 首次缩放
+            int width = (int) (readWidth * scale);
+            int height = (int) (readHeight * scale);
+            // 宽高比 面积 limit
+            Double area = Double.valueOf(width * height);
+            Double wh = Double.valueOf(width / height);
+            Double limit = Double.valueOf(1440 * 1080);
+
+            // 缩放完成，若还有一边超出范围 继续缩放
+            while (area > limit) {
+                // 二次缩放
+                scales = limit / area;
+                BigDecimal bigDecimal = new BigDecimal(scales);
+                scales = bigDecimal.setScale(4, BigDecimal.ROUND_HALF_UP).doubleValue();
+
+                x = Math.sqrt(scales / wh);
+                bigDecimal = new BigDecimal(x);
+                x = bigDecimal.setScale(4, BigDecimal.ROUND_HALF_UP).doubleValue();
+
+
+                y = Math.sqrt(scales / wh);
+                bigDecimal = new BigDecimal(y);
+                y = bigDecimal.setScale(4, BigDecimal.ROUND_HALF_UP).doubleValue();
+
+                width *= (int) Math.floor(x * wh);
+                height *= (int) Math.floor(y);
+                area = Double.valueOf(width*height);
+            }
+
 
 
             // --------------------cv核心代码段start--------------
@@ -200,7 +243,7 @@ public class ImageUtils {
 
             // 根据pathName判断是本地上传还是url上传
             File copyFile = (pathName.contains(System.getProperty("user.dir"))) ?
-                    new File("scalingPre_" +name) : new File(pathName+"scaling_" +name);
+                    new File("scaling3_" +name) : new File(pathName+"scaling_" +name);
 
             // 核心代码 bufferedImage写入copyFile
             ImageIO.write(bufferedImage,formateName, copyFile);
@@ -210,7 +253,6 @@ public class ImageUtils {
         }
         return null;
     }
-
     /**
      * Url图片
      * 1.将Url ->InputStream ->本地File
@@ -302,4 +344,40 @@ public class ImageUtils {
         }
         return null;
     }
+
+    /**
+     * 无损压缩图片(大小上)的算法的Java实现代码
+     */
+    public static void compressImageInByte(File srcFile) throws IOException {
+//        String path = srcFile.getPath();
+        String name = srcFile.getName();
+        // 读取图片文件
+        Image srcImage = ImageIO.read(srcFile);
+
+        // 原图宽度
+        int width = srcImage.getWidth(null);
+        // 原图高度
+        int height = srcImage.getHeight(null);
+
+        // 压缩比例
+        float ratio = 1.0f;
+        // 压缩比例为原图的1/2
+        ratio = Math.max(ratio, 0.5f);
+        // 压缩比例不能超过1
+        ratio = Math.min(ratio, 1.0f);
+
+        // 压缩后的宽度
+        int destWidth = (int) (width * ratio);
+        // 压缩后的高度
+        int destHeight = (int) (height * ratio);
+
+        // 压缩后的图片
+        BufferedImage destImage = new BufferedImage(destWidth, destHeight, BufferedImage.TYPE_INT_RGB);
+        destImage.getGraphics().drawImage(srcImage, 0, 0, destWidth, destHeight, null);
+
+        // 将压缩后的图片写入文件
+        File destFile = new File(name);
+        ImageIO.write(destImage, "jpg", destFile);
+    }
+
 }
